7629c8c32dc2c4ad7aa3d59153ca0904cc14c71b
KmGun <gunpoll823@gmail.com>
2022-07-08 23:21:41 +0900

    220708 SPA routing 공부 완료_구현은 내일

---
+++ client/js/pages/logout.js
@@ -0,0 +1,10 @@
+export default class {
+    constructor() {
+        document.title = "logout";
+    }
+    async getHtml() {
+        return `
+            <h1>This is logout Page</h1>
+        `;
+    }
+}
---
+++ client/js/pages/register.js
@@ -0,0 +1,10 @@
+export default class {
+    constructor() {
+        document.title = "register";
+    }
+    async getHtml() {
+        return `
+            <h1>This is register Page</h1>
+        `;
+    }
+}
